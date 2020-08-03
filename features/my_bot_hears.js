3b6fb4befbc7b135c91c7062e97d6778b6e2f127
Heather Ransome <siobahnv@gmail.com>
2020-08-03 10:14:21 -0700

    Learning regular expressions, woo.

--- features/my_bot_hears.js
+++ features/my_bot_hears.js
@@ -9,4 +9,13 @@ module.exports = function(controller) {
         await bot.reply(message, "Hi, I'm Miss Pibbles!");
     });
 
+    controller.hears(async (message) => message.text && message.text.toLowerCase() === 'weather', ['message'], async (bot, message) => {
+        await bot.reply(message, "Hi, I'm Miss Pibbles!");
+    });
+
+    // use a regular expression to match the text of the message
+    controller.hears(new RegExp(/^weather/), ['message','direct_message'], async function(bot, message) {
+        await bot.reply(message,{ text: 'The weather on Mars is fantastic!' });
+    });
+
 }
